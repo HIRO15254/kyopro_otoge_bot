76dfa278782bab49483783eeff4b771a19a36eee
HIRO15254 <hi089697@docomo.ne.jp>
2021-11-26 21:51:44 +0900

    ping作った

---
+++ Main.js
@@ -0,0 +1,43 @@
+const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const token = process.env.DISCORD_TOKEN;
+const guildID = process.env.GUILD_ID;
+// const clientID = process.env.CLIENT_ID;
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const commands = {};
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands[command.data.name] = command;
+}
+
+client.once('ready', async () => {
+	const data = [];
+	for (const commandName in commands) {
+		data.push(commands[commandName].data);
+	}
+	await client.application.commands.set(data, guildID);
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) {
+		return;
+	}
+	const command = commands[interaction.commandName];
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: 'There was an error while executing this command!',
+			ephemeral: true,
+		});
+	}
+});
+
+client.login(token);
---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+module.exports = {
+	data: {
+		name: 'ping',
+		description: 'Replies with Pong!',
+	},
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
